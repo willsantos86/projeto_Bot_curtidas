943a9b71b8a1fb80d1721166a4298d8fcc616ec2
William Santos <willsantos.edf@gmail.com>
2022-12-14 00:11:26 -0300

    Primeiro commit.

---
+++ bot_curtidas.py
@@ -0,0 +1,19 @@
+'''Bot de curtidas e comantários no Instagram com PyAutoGui
+
+Criar um bot que define qual pagina quer seguir, verifica se a postagem mais atual ainda não foi curtida pelo bot.
+Caso uma nova postagem tenha sido feita, ele deve entrar nessa postagem, curtir e comentar algo nela
+1 - Navegar até o site https://www.instagram.com
+2- Entrar com meu usuário
+3- Entrar com a minha senha
+4- Clicar em "login"
+5- Clicar em "not now"
+6- Fechar janela de "salvar senha"
+7- Pesquisar pela pagina
+8- Entrar na pagina
+9- Clicar na postagem
+10- Verificar se já curtiu ou não aquela foto
+11-Se já tiver curtido, fazer nada e pausar o bot por 24 horas
+12-Se não tiver curtido, curtir a foto
+13- Se não tiver comentado, comentar na foto
+14-Pausar por 24 horas
+15-Após 24 horas fazer tudo de novo.'''
